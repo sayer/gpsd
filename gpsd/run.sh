5e7dff0c71e233b16e1ab7e3cc7440b05a7a8596
Stephen Ayers <sayer@ayersnet.com>
2024-12-13 16:22:06 -0700

    test

--- gpsd/run.sh
+++ gpsd/run.sh
@@ -6,9 +6,6 @@ echo "\n\n\nstarting gpsd: ${OPTIONS} ${DEVICE}"
 # Start gpsd in the foreground
 /usr/sbin/gpsd ${OPTIONS} ${DEVICE}
 
-#echo "try again...."
-#/usr/sbin/gpsd -b -N -n /dev/ttyACM0
-
 # Wait for any process to exit
 wait -n
 
