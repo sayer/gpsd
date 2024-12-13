7c92c8d1a8aa44cfa7748fa5fefc30713a5fff0d
sayer <sayer@ayersnet.com>
2024-12-13 14:49:56 -0700

    maybe vars will now be passed

--- gpsd/run.sh
+++ gpsd/run.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/with-contenv bash
 
-# Set gpsd options from environment variables, or defaults
-#GPS_DEVICE=$(bashio::config 'device')
-#GPS_OPTIONS=$(bashio::config 'options')
-
 env
 
 modprobe pps_core
 
 echo "starting gpsd: ${OPTIONS} - ${DEVICE}"
-bashio::log.info "Starting gpsd : ${OPTIONS} - ${DEVICE}"
 
 # Start gpsd in the foreground
 /usr/sbin/gpsd ${OPTIONS} ${DEVICE}
 
-echo "try again...."
-/usr/sbin/gpsd -b -N -n /dev/ttyACM0
+#echo "try again...."
+#/usr/sbin/gpsd -b -N -n /dev/ttyACM0
 
 # Wait for any process to exit
 #wait -n
