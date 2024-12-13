8783cfc0c1be1b3c0bbcddab065bedd89279b8d8
sayer <sayer@ayersnet.com>
2024-12-13 10:01:01 -0700

    i hope it works..

--- gpsd/run.sh
+++ gpsd/run.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 # Set gpsd options from environment variables, or defaults
-GPS_DEVICE="${GPS_DEVICE:-/dev/ttyACM0}"
-GPS_OPTIONS="${GPS_OPTIONS:-}"
+#GPS_DEVICE="${GPS_DEVICE:-/dev/ttyACM0}"
+#GPS_OPTIONS="${GPS_OPTIONS:-}"
 
-echo "starting gpsd: $GPS_OPTIONS $GPS_DEVICE"
+ls -l /usr/sbin/gpsd
+
+echo "starting gpsd: ${GPS_OPTIONS} ${GPS_DEVICE}"
 
 # Start gpsd in the foreground
 /usr/sbin/gpsd ${GPS_OPTIONS} ${GPS_DEVICE}
 
 # Wait for any process to exit
-wait -n
+#wait -n
 
 echo "gpsd exited"
 
