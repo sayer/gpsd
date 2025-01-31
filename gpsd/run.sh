bbe92b3bb3bec2557ff00b81d761deef881742c1
Stephen Ayers <sayer@ayersnet.com>
2025-01-31 14:21:59 -0700

    remove hardcoded device

--- gpsd/run.sh
+++ gpsd/run.sh
@@ -4,7 +4,7 @@ echo -e "\n\n\nstarting gpsd: ${OPTIONS} ${DEVICE}"
 date
 
 # Start gpsd in the foreground
-/usr/sbin/gpsd ${OPTIONS} ${DEVICE} || exit 1
+/usr/sbin/gpsd "${OPTIONS}" "${DEVICE}" || exit 1
 
 # Wait for any process to exit
 wait -n
